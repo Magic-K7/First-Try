8ed216c0b1b1884eb6b42b5888580f0c7de8256d
Kristina Marinova <148051654+Magic-K7@users.noreply.github.com>
2023-10-27 19:02:41 +0300

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,30 @@
+//Hogwarts Sorting Hat
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void main1() {
+
+  int number=0;
+
+  cout << "\nI am the Hogwarts Sorting Hat.\n\n";
+  cout << "Enter a number from 1 to 4 to find out which House you belong to.\n\n";
+  cin >> number;
+
+    if (number<1 || number>4)
+	{
+		cout << "Invalid input. Please enter a number between 1 and 4.\n";
+	}
+    else if (number == 1) {
+      cout << "You are a member of Hufflepuff\n";
+    } 
+	else if (number == 2) {
+    cout << "You are a member of Gryffindor\n";
+    } 
+	else if (number == 3) {
+    cout << "You are a member of Slytherin\n";
+    } 
+	else {
+      cout << "You are a member of Ravenclaw\n"<<endl; 
+     } 
+	getch(); 
+  }
